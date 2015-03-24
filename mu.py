11795a4427503e637ace7381e15d06f86eef5769
AbrahamDominguez <a2doming@uwaterloo.ca>
2015-03-24 14:02:54 -0400

    Create mu.py

---
+++ mu.py
@@ -0,0 +1,21 @@
+X = 0.73
+Y = 0.25
+Z = 0.02
+
+mu = 1.0/(2.0*X + 0.75*Y + 0.5*Z)
+
+def iterate():
+    global mu, X, Y, Z
+    mu = 1.0/(2.0*X + 0.75*Y + 0.5*Z)
+
+def get_mu():
+    return mu
+
+def get_X():
+    return X
+
+def get_Y():
+    return Y
+
+def get_Z():
+    return Z
